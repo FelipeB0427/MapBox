bbdc4ba39dc07990c8c0e2322333ad7ab019ecef
Felipe Brigagão de Almeida <felipe-almeida2012@hotail.com>
2022-06-28 21:28:57 -0300

    Initial commit

---
+++ Mapbox/Mapbox/MapboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapboxApp.swift
+//  Mapbox
+//
+//  Created by Felipe Brigagão de Almeida on 28/06/22.
+//
+
+import SwiftUI
+
+@main
+struct MapboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
